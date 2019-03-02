e9f5e9a6bd5ac8cc8bc7c0e412b4aa0ac1cd57b4
Fahim Kabir <fkabir101@gmail.com>
2019-03-02 18:07:55 -0500

    shuffle working

--- src/components/card/imageCard.js
+++ src/components/card/imageCard.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './style.css';
 function ImageCard(props){
   return(
-    <div className = "card card-overide">
+    <div className = "card card-overide" id ={props.id}>
       <div className = "card-body">
         <img src={props.image} alt="Cat"/>
-        <button type="button" class="btn btn-primary btn-large">Click</button>
+        <button type="button" className="btn btn-primary btn-large" onClick={props.onValueChanged} id ={props.id}>Click</button>
       </div>
     </div>
   )
--- src/components/clicker.js
+++ src/components/clicker.js
@@ -3,18 +3,39 @@ import imageRref from '../images/image-ref.json'
 import ImageCard from './card/imageCard';
 import './style.css'
 
+
 class Clicker extends Component{
   state = {
     imageObject : imageRref
   }
+
+  onValueChanged = (event) => {
+    if(this.state.imageObject[event.target.id].clicked === false){
+      let tempObject = this.state.imageObject;
+      tempObject[event.target.id].clicked = true;
+      
+      for (let i = tempObject.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [tempObject[i], tempObject[j]] = [tempObject[j], tempObject[i]];
+      }
+
+      this.setState({imageObject : tempObject})
+    }
+    else{
+      alert("Already True")
+    }
+  }
+
+
   render(){
-    console.log(this.state.imageObject);
     return(
       <div className = "wrapper">
         {this.state.imageObject.map(image => (
           <ImageCard
             key={image.id}
+            id={image.id}
             image={image.ref}
+            onValueChanged={this.onValueChanged}
           />
         ))}
       </div>
