f73df0d56b02fc79eff49df3c530ea910d610c65
Fahim Kabir <fkabir101@gmail.com>
2019-03-02 12:46:22 -0500

    image json done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Clicker from "./components/clicker"
+
+class App extends Component {
+  render() {
+    return (
+      <Clicker/>
+    );
+  }
+}
+
+export default App;
