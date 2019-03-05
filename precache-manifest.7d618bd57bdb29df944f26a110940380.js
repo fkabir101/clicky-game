9d1f0d5466ed5b2055684997d2d0cf71f71fe4e4
Fahim Kabir <fkabir101@gmail.com>
2019-03-04 21:47:30 -0500

    Updates

---
+++ precache-manifest.7d618bd57bdb29df944f26a110940380.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "91777259bda24af894fa",
+    "url": "/clicky-game/static/js/main.91777259.chunk.js"
+  },
+  {
+    "revision": "573698287b10209aaf23",
+    "url": "/clicky-game/static/js/2.57369828.chunk.js"
+  },
+  {
+    "revision": "91777259bda24af894fa",
+    "url": "/clicky-game/static/css/main.16208b74.chunk.css"
+  },
+  {
+    "revision": "8b111a655f96710a472d9f683cc160b1",
+    "url": "/clicky-game/index.html"
+  }
+];
